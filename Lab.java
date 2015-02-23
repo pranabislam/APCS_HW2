dd54bf5234c58181254eb5a5967952318dd848fd
Pranab Islam <islampranab@gmail.com>
2015-02-23 08:40:06 -0500

    Adasd

---
+++ Lab.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Lab{
+    public int[][] lake = new int[4][6];
+    public Random rd = new Random();
+    
+    public Lab(){
+	for (int i = 0; i<lake.length;i++){
+	    for (int k = 0; k < lake[0].length;k++){
+		lake[i][k] = rand.nextInt(11) + 20;
+	    }}
+    }
+       
+    public int makeLake(int r, int c, int e, int n){
+	
+
+    }
