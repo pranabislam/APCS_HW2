c3eec78bd37a2ed3c0b028a0f82ca5ad322e1791
Pranab <islampranab@gmail.com>
2015-03-23 02:20:36 -0400

    adsdadas

--- 09-Iterator/LLit.java
+++ 09-Iterator/LLit.java
@@ -2,22 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class LLit<E> implements Iterator<E>{
-		private Node<E> t;
-		public LLit(Node<E> n){
-				t=n;
-		}
+    private Node<E> t;
+    private Node<E> head;
 
-		public boolean hasNext(){
-				return t!=null;
-		}
+    public LLit(Node<E> n){
+	t=n;
+	head = n;
+    }
 
-		public E next(){
-				E retval = t.getData();
-				t=t.getNext();
-				return retval;
-		}
+    public boolean hasNext(){
+	return t!=null;
+    }
 
-		public void remove() {
-				// fill this in later
-		}
+    public E next(){
+	E retval = t.getData();
+	t=t.getNext();
+	return retval;
+    }
+
+    public void remove() {
+	Node tmp= head;
+	while(tmp.getNext().getNext()!=null){
+	    if(tmp.getNext().getNext() == t){
+		tmp.setNext(t);
+	    }
+	    tmp = tmp.getNext();
+	}
+    }
 }
