a3134edac91d0f4ceb4d80257fed4cef6fb4b08f
Pranab Islam <islampranab@gmail.com>
2015-04-24 08:23:44 -0400

    Adsd

--- 14-Trees/Tree.java
+++ 14-Trees/Tree.java
@@ -3,14 +3,33 @@ public class Tree{
     private Node t2; // dummy 2
 
 
-    public void Insert(Node n){
+    
+
+    public void Insert(Node n){	
+	while (t1 =! null){
+	    
+	    int c = t1.getData().compareTo(n.getData());
+	    if (c > 0){
+		t2 = t1;
+		t1 = t1.getRight();
+		
+	    }
+	    if (c < 0){
+		t2 = t1;
+		t1 = t1.getLeft();
+		}
+	}
+	if (t2.getData().compareTo(n.getData()) > 0){
+	    t2.setLeft(new Node(i));
+	}
+        else t2.setRight(new Node(i));
 	
 	
     }
 
     public Node Search(Node t, Integer i){
 	while (t =! null){
-	    int c = t.getData.compareTo(i);
+	    int c = t.getData().compareTo(i);
 	    if (c > 0){
 		t = t.getRight();
 	    }
