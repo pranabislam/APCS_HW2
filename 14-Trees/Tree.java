91529ff1ad041f77a43ecc83c8ea1de5b7c777d1
Pranab <islampranab@gmail.com>
2015-04-23 22:36:04 -0400

    adasd

--- 14-Trees/Tree.java
+++ 14-Trees/Tree.java
@@ -8,8 +8,22 @@ public class Tree{
 	
     }
 
-
-
-
+    public Node Search(Node t, Integer i){
+	while (t =! null){
+	    int c = t.getData.compareTo(i);
+	    if (c > 0){
+		t = t.getRight();
+	    }
+	    if (c < 0){
+		t = t.getLeft();
+	    }
+	    else return t;
+	}
+	return null;
+    }
+	
+	
 }
+
+
 
